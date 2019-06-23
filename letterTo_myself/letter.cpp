c1dcf972a59b231d70fca74ac259bc874e817823
Vincent Iroleh <irolehiroleh@gmail.com>
2019-06-23 23:04:34 +0100

    (add letter)add letter to myself program

---
+++ letterTo_myself/letter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  
+  cout << "Dear Self,\n";
+  cout << "Never give up on your goals towards becoming a worldclass Software Engineer,\n";
+  cout << "Remember that nothing good comes easy,\n";
+  cout << "Learn about your craft everyday,\n";
+  cout << "Learn from everyone and everything,\n";
+  cout << "Teach people you're better than,\n";
+  cout << "And utilize every opportunity that will make you a better Software Engineer.\n\n";
+  cout << "Yours Self,\n\n";
+  cout << "Vincent Iroleh\n";
+  cout << "Sun Jun 23, 2019";
+}
