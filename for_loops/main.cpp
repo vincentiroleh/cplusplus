0613062b9864200d48adfeb3c606766191a441d8
Vincent Iroleh <irolehiroleh@gmail.com>
2019-07-03 18:59:29 +0100

    add for loops

---
+++ for_loops/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nums[] = {1,2,3,5,6,7,8,9};
+
+    for(int i = 0; i < nums[i]; i++) {
+        cout << nums[i] << endl;
+    }
+    return 0;
+}
