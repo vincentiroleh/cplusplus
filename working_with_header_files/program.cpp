4a7125145d68ed9054740759a5fb1f5efc73e084
Vincent Iroleh <irolehiroleh@gmail.com>
2019-07-10 14:32:25 +0100

    working with header files

---
+++ working_with_header_files/program.cpp
@@ -0,0 +1,15 @@
+/*Goal: practice using header files.
+**. 
+**Put in the header file every thing that is related to 
+**"how to do a task".
+**Put the "what to do" in the main program.
+*/
+
+#include "program.hpp"
+
+int main()
+{
+    cout << "Hello, I use header files!\n";
+
+    return 0;
+}
---
+++ working_with_header_files/program.hpp
@@ -0,0 +1,8 @@
+/*This is a header file. 
+**Put in a header file every thing that is related to 
+**"how to do a task".
+**Put the "what to do" in the main program.
+*/
+
+#include <iostream>
+using namespace std;
